9d6a726e43e1a94f1513d4c87f453377fa9d3405
Kasper Rune Søgaard <kasper@sogaard.us>
2021-03-01 22:29:34 -0800

    Signle build (#10)
    * Single build file
    
    * gradle.build file clean up
    
    * Fixed dependency and harbor url

--- maui/src/build/archipelago/maui/utils/ConfigUtil.java
+++ maui/src/build/archipelago/maui/utils/ConfigUtil.java
@@ -6,11 +6,9 @@ import java.nio.file.*;
 public class ConfigUtil {
     public static void writeDefaultConfig(Path file) throws IOException {
         String defaultConfig = "" +
-                "services.versionset.url: \"http://localhost:8081\"\n" +
-                "services.packages.url: \"http://localhost:8080\"\n" +
-                "oauth.endpoint: \"http://localhost:8087\"\n" +
-                "oauth.clientid: \"IGrxIc1VpifO5qZPvkWN\"\n" +
-                "services.harbor.url: \"http://localhost:8082\"\n" +
+                "oauth.endpoint: http://localhost:8087\n" +
+                "oauth.clientid: IGrxIc1VpifO5qZPvkWN\n" +
+                "services.harbor.url: http://localhost:8093\n" +
                 "sync.threads: 5\n" +
                 "";
 
