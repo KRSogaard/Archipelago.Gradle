a2a4336d02cd582577f29c5fc1e2833e20c404da
Kasper Sogaard <kasper@sogaard.us>
2021-04-08 10:39:54 -0700

    Update to HTTPS

---
+++ build-server-builder/src/build/archipelago/buildserver/builder/controllers/HealthController.java
@@ -0,0 +1,19 @@
+package build.archipelago.buildserver.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class HealthController {
+
+    protected static final String HEALTHY_REPLY = "Healthy";
+    public static final String HEALTH_PATH = "/health-check";
+
+    @GetMapping(value = {HEALTH_PATH})
+    @ResponseStatus(HttpStatus.OK)
+    public String doHealthCheck() {
+        return HEALTHY_REPLY;
+    }
+}
